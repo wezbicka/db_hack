40edfedf198571184680ed183e35c48aa0728ac0
Polina <75391420+wezbicka@users.noreply.github.com>
2022-11-05 14:08:58 +0300

    Got infotmation about child

--- scripts.py
+++ scripts.py
@@ -2,3 +2,5 @@ from datacenter.models import Schoolkid
 
 students = Schoolkid.objects.all()
 print(students)
+child = Schoolkid.objects.filter(full_name__contains="Фролов Иван")[0]
+print(child.full_name)
