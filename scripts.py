0b101f701b2934b9315daf9d99225ae9a9708223
Polina <75391420+wezbicka@users.noreply.github.com>
2022-11-05 15:07:28 +0300

    Added logging

--- scripts.py
+++ scripts.py
@@ -1,12 +1,25 @@
+import logging
+import sys
+
 from datacenter.models import Schoolkid
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger('Как назвать логгер?')
 
 
 def get_kid_by_name(child_name: str) -> Schoolkid:
     try:
         child = Schoolkid.objects.get(full_name__contains=child_name)
-    except MultipleObjectsReturned:
-        print("Найдено больше одного ученика")
-    except ObjectDoesNotExist:
-        print("Не найдено учеников с таким именем")
-    return child
+    except MultipleObjectsReturned as exeption:
+        logger.error(f'{exeption}.Найдено больше одного ученика')
+        sys.exit()
+    except ObjectDoesNotExist as exeption:
+        logger.error(f'{exeption}.Не найдено учеников с таким именем')
+        sys.exit()
+    else:
+        logger.info(f'Ученик по имени "{child.full_name}" найден.')
+        return child
+
+
+
