550859556dc1c5bce305dc039ed3ca6ef253103e
Polina <75391420+wezbicka@users.noreply.github.com>
2022-11-05 14:15:27 +0300

    Added get_kid_by_name

--- scripts.py
+++ scripts.py
@@ -1,6 +1,12 @@
 from datacenter.models import Schoolkid
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
-students = Schoolkid.objects.all()
-print(students)
-child = Schoolkid.objects.filter(full_name__contains="Фролов Иван")[0]
-print(child.full_name)
+
+def get_kid_by_name(child_name: str) -> Schoolkid:
+    try:
+        child = Schoolkid.objects.get(full_name__contains=child_name)
+    except MultipleObjectsReturned:
+        print("Найдено больше одного ученика")
+    except ObjectDoesNotExist:
+        print("Не найдено учеников с таким именем")
+    return child
